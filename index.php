9cad366fd66b117d64aadc1f925ad86a727c8963
Vini Moraes <redvini@gmail.com>
2021-09-21 15:07:07 -0300

    Alterei o arquivo index.php e criei o processa.php

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
-<Doctype></Doctype>
+<!Doctype htnl></Doctype>
+<><html lang="pt-br">
+    <head>
+        <title>CRUD - Cadastrar Criar Ler Atualizar e Deletar</title>
+    </head>
+        <meta charset="utf-8">
+        <body>
+            <h1>
+                Cadastrar Usuário
+            </h1>
+            <form method="POST" action="processa.php">
+           <label>Nome:</label>
+           <input type="text" name="nome" placeholder="Digite o nome completo"><br><br><input>     
+
+            </form>
+        </body>
+</html>
